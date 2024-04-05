87d6fa77f9eed656250465693f10641a1b41046e
Dr.-Ing. Amilcar do Carmo Lucas <amilcar.lucas@iav.de>
2024-04-05 15:05:25 +0200

    Added credits and licenses

--- README.md
+++ README.md
@@ -52,31 +52,4 @@ To use and develop this software you must obey the [ArduPilot Methodic Configura
 ## License
 
 This project is licensed under the GNU General Public License v3.0. See the [LICENSE](LICENSE) file for details.
-
-It directly uses:
-
-| Software | License |
-|----------|---------|
-| [tkinter](https://docs.python.org/3/library/tkinter.html) | [Python Software Foundation License](https://docs.python.org/3/license.html) |
-| [argparse](https://docs.python.org/3/library/argparse.html) | [Python Software Foundation License](https://docs.python.org/3/license.html) |
-| [logging](https://docs.python.org/3/library/logging.html) | [Python Software Foundation License](https://docs.python.org/3/license.html) |
-| [typing](https://docs.python.org/3/library/typing.html) | [Python Software Foundation License](https://docs.python.org/3/license.html) |
-| [json](https://docs.python.org/3/library/json.html) | [Python Software Foundation License](https://docs.python.org/3/license.html) |
-| [os](https://docs.python.org/3/library/os.html) | [Python Software Foundation License](https://docs.python.org/3/license.html) |
-| [re](https://docs.python.org/3/library/re.html) | [Python Software Foundation License](https://docs.python.org/3/license.html) |
-| [webbrowser](https://docs.python.org/3/library/webbrowser.html) | [Python Software Foundation License](https://docs.python.org/3/license.html) |
-| [pymavlink](https://github.com/ArduPilot/pymavlink) | [GNU Lesser General Public License v3.0](https://github.com/ArduPilot/pymavlink/blob/master/COPYING) |
-| [pyserial](https://pyserial.readthedocs.io/en/latest/pyserial.html) | [BSD License](https://github.com/pyserial/pyserial/blob/master/LICENSE.txt) |
-| [Scrollable TK frame](https://gist.github.com/mp035/9f2027c3ef9172264532fcd6262f3b01) by Mark Pointing | [Mozilla Public License, v. 2.0](https://mozilla.org/MPL/2.0/) |
-| [Python Tkinter ComboBox](https://dev.to/geraldew/python-tkinter-an-exercise-in-wrapping-the-combobox-ndb) by geraldew | [Mozilla Public License, v. 2.0](https://mozilla.org/MPL/2.0/) |
-
-It indirectly uses:
-
-| Software | License |
-|----------|---------|
-| [certifi](https://github.com/certifi/python-certifi) | [Mozilla Public License 2.0](https://github.com/certifi/python-certifi/blob/master/LICENSE) |
-| [charset-normalizer](https://github.com/Ousret/charset_normalizer) | [MIT License](https://github.com/Ousret/charset_normalizer/blob/master/LICENSE) |
-| [future](https://github.com/PythonCharmers/python-future) | [MIT License](https://github.com/PythonCharmers/python-future/blob/master/LICENSE.txt) |
-| [urllib3](https://github.com/urllib3/urllib3) | [MIT License](https://github.com/urllib3/urllib3/blob/main/LICENSE.txt) |
-| [lxml](https://github.com/lxml/lxml) | [BSD License](https://github.com/lxml/lxml/blob/master/LICENSE.txt) |
-| [idna](https://github.com/kjd/idna) | [BSD License](https://github.com/kjd/idna/blob/master/LICENSE.md) |
+It builds upon other [opensource software packages](credits/CREDITS.md)
---
+++ credits/update-credit-licenses.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+'''
+This script downloads the licenses of the direct and indirect dependencies of the project
+
+This file is part of Ardupilot methodic configurator. https://github.com/ArduPilot/MethodicConfigurator
+
+(C) 2024 Amilcar do Carmo Lucas, IAV GmbH
+
+SPDX-License-Identifier:    GPL-3
+'''
+
+import requests
+
+
+# List of direct_dependencies and their license URLs
+direct_dependencies = [
+    # {"name": "tkinter", "license_url": "https://docs.python.org/3/license.html"},
+    # {"name": "argparse", "license_url": "https://docs.python.org/3/license.html"},
+    # {"name": "logging", "license_url": "https://docs.python.org/3/license.html"},
+    # {"name": "typing", "license_url": "https://docs.python.org/3/license.html"},
+    # {"name": "json", "license_url": "https://docs.python.org/3/license.html"},
+    # {"name": "os", "license_url": "https://docs.python.org/3/license.html"},
+    # {"name": "re", "license_url": "https://docs.python.org/3/license.html"},
+    # {"name": "webbrowser", "license_url": "https://docs.python.org/3/license.html"},
+    {"name": "pymavlink", "license_url": "https://raw.githubusercontent.com/ArduPilot/pymavlink/master/COPYING"},
+    {"name": "pyserial", "license_url": "https://raw.githubusercontent.com/pyserial/pyserial/master/LICENSE.txt"},
+    {"name": "Scrollable_TK_frame", "license_url": "https://mozilla.org/MPL/2.0/"},
+    {"name": "Python_Tkinter_ComboBox", "license_url": "https://mozilla.org/MPL/2.0/"},
+]
+
+
+# List of direct_dependencies and their license URLs
+indirect_dependencies = [
+    {"name": "certifi", "license_url": "https://raw.githubusercontent.com/certifi/python-certifi/master/LICENSE"},
+    {"name": "charset-normalizer",
+     "license_url": "https://raw.githubusercontent.com/Ousret/charset_normalizer/master/LICENSE"},
+    {"name": "future", "license_url": "https://raw.githubusercontent.com/PythonCharmers/python-future/master/LICENSE.txt"},
+    {"name": "urllib3", "license_url": "https://raw.githubusercontent.com/urllib3/urllib3/main/LICENSE.txt"},
+    {"name": "lxml", "license_url": "https://raw.githubusercontent.com/lxml/lxml/master/LICENSE.txt"},
+    {"name": "idna", "license_url": "https://raw.githubusercontent.com/kjd/idna/master/LICENSE.md"}
+]
+
+
+def download_license(package_name, license_url):
+    try:
+        response = requests.get(license_url)
+        response.raise_for_status() # Raise an exception if the request failed
+        # Use a fixed filename for the Mozilla Public License version 2.0
+        if package_name in ["Scrollable_TK_frame", "Python_Tkinter_ComboBox"]:
+            filename = f"{package_name}-Mozilla_Public_License_version_2.0.html"
+        else:
+            filename = f"{package_name}-{license_url.split('/')[-1]}"
+        with open(filename, 'wb') as f:
+            f.write(response.content)
+        print(f"Downloaded {filename}")
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to download {package_name} license: {e}")
+
+
+# Download each package's license
+for package in direct_dependencies + indirect_dependencies:
+    download_license(package['name'].replace(' ', '_'), package['license_url'])
