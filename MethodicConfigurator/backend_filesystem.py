f4a3c1e28cc5f99fdf23c2bc2b183cbe4de81223
Dr.-Ing. Amilcar do Carmo Lucas <amilcar.lucas@iav.de>
2024-08-27 11:52:38 +0200

    BUGFIX: do not copy apm.pdef.xml files from templates. They are probably not aplicable to the new vehicle

--- MethodicConfigurator/backend_filesystem.py
+++ MethodicConfigurator/backend_filesystem.py
@@ -434,7 +434,7 @@ class LocalFilesystem(VehicleComponents, ConfigurationSteps, ProgramSettings):  
     def copy_template_files_to_new_vehicle_dir(self, template_dir: str, new_vehicle_dir: str):
         # Copy the template files to the new vehicle directory
         for item in os_listdir(template_dir):
-            if item == 'vehicle.jpg':
+            if item in ['vehicle.jpg', 'apm.pdef.xml']:
                 continue
             s = os_path.join(template_dir, item)
             d = os_path.join(new_vehicle_dir, item)
