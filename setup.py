70a467b26c44f2e46965a49776eaef6c1805d310
Dr.-Ing. Amilcar do Carmo Lucas <amilcar.lucas@iav.de>
2024-11-11 17:49:11 +0100

    FIX: attempt to fix inclusion of vehicle template files on pypi.org

--- setup.py
+++ setup.py
@@ -10,8 +10,10 @@ SPDX-FileCopyrightText: 2024 Amilcar do Carmo Lucas <amilcar.lucas@iav.de>
 SPDX-License-Identifier: GPL-3.0-or-later
 """
 
+import fnmatch
 import os
 import shutil
+from typing import List, Tuple
 
 from setuptools import find_packages, setup
 
@@ -58,6 +60,24 @@ with open("README.md", encoding="utf-8") as f:
     long_description = long_description.replace(
         "images/App_screenshot1.png", f"{PRJ_URL}/raw/master/images/App_screenshot1.png"
     )
+
+
+# recursively find all files that match the globs and return tuples with their directory and a list of relative paths
+def find_data_files(path: str, globs: List[str]) -> Tuple[str, List[str]]:
+    data_files_path = os.path.join("MethodicConfigurator", path)
+    data_files_path_base = os.path.join(os.path.dirname(__file__), "MethodicConfigurator")
+    ret = []
+    for dirpath, _dirnames, filenames in os.walk(data_files_path):
+        data_files = []
+        for glob in globs:
+            for filename in fnmatch.filter(filenames, glob):
+                relative_path = os.path.join(dirpath, filename)
+                data_files.append(relative_path)
+        if data_files:
+            ret.append((os.path.relpath(dirpath, data_files_path_base), data_files))
+
+    return ret
+
 
 # So that the vehicle_templates directory contents get correctly read by the MANIFEST.in file
 TEMPLATES_SRC_DIR = "vehicle_templates"
@@ -125,8 +145,16 @@ setup(
     license="GPLv3",
     python_requires=">=3.6",
     keywords=["ArduPilot", "Configuration", "SCM", "Methodic", "ArduCopter", "ArduPlane", "ArduRover", "ArduSub"],
-    # package_dir={"": "MethodicConfigurator"}, this unfortunatly breaks the build
-    include_package_data=True,
+    # this is used by sdist
+    package_data={
+        "MethodicConfigurator": ["*.param", "*.jpg", "*.json", "*.xml", "*.mo", "*.png"],
+    },
+    exclude_package_data={"MethodicConfigurator": ["test.xml"]},
+    # this is used by bdist
+    data_files=[
+        *find_data_files("vehicle_templates", ["*.param", "*.jpg", "*.json", "*.xml"]),
+        *find_data_files("locale", ["*.mo"]),
+    ],
     scripts=extra_scripts,
     # Specify entry points for command-line scripts
     entry_points={
