4d788d3c5c2e2d001ecf0fc1c7ffd0063461d03d
Dr.-Ing. Amilcar do Carmo Lucas <amilcar.lucas@iav.de>
2025-01-06 22:14:01 +0100

    IMPROVEMENT: remove unused code in setup.py

--- setup.py
+++ setup.py
@@ -15,27 +15,6 @@ import os
 
 from setuptools import setup
 
-PRJ_URL = "https://github.com/ArduPilot/MethodicConfigurator"
-
-# Read the long description from the README file
-with open("README.md", encoding="utf-8") as f:
-    long_description = f.read()
-    # Use Absolute links so that the pyPI page renders correctly
-    long_description = long_description.replace("(USERMANUAL.md", f"({PRJ_URL}/blob/master/USERMANUAL.md")
-    long_description = long_description.replace("(QUICKSTART.md", f"({PRJ_URL}/blob/master/QUICKSTART.md")
-    long_description = long_description.replace("(CONTRIBUTING.md", f"({PRJ_URL}/blob/master/CONTRIBUTING.md")
-    long_description = long_description.replace("(ARCHITECTURE.md", f"({PRJ_URL}/blob/master/ARCHITECTURE.md")
-    long_description = long_description.replace("(CODE_OF_CONDUCT.md", f"({PRJ_URL}/blob/master/CODE_OF_CONDUCT.md")
-    long_description = long_description.replace("(LICENSE.md", f"({PRJ_URL}/blob/master/LICENSE.md")
-    long_description = long_description.replace("(USECASES.md", f"({PRJ_URL}/blob/master/USECASES.md")
-    long_description = long_description.replace("(credits/CREDITS.md", f"({PRJ_URL}/blob/master/credits/CREDITS.md")
-    long_description = long_description.replace(
-        "images/when_to_use_amc.png", f"{PRJ_URL}/raw/master/images/when_to_use_amc.png"
-    )
-    long_description = long_description.replace(
-        "images/App_screenshot1.png", f"{PRJ_URL}/raw/master/images/App_screenshot1.png"
-    )
-
 
 # recursively find all files that match the globs and return tuples with their directory and a list of relative paths
 def find_data_files(path: str, globs: list[str]) -> list[tuple[str, list[str]]]:
@@ -55,8 +34,6 @@ def find_data_files(path: str, globs: list[str]) -> list[tuple[str, list[str]]]:
 
 setup(
     packages=["ardupilot_methodic_configurator"],
-    # long_description=long_description,
-    # long_description_content_type="text/markdown",
     # this is used by bdist
     data_files=[
         *find_data_files(
