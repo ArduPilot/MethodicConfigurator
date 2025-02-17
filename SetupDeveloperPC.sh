5b8195d5bff55317d383caa26aab456aa250c442
Dr.-Ing. Amilcar do Carmo Lucas <amilcar.lucas@iav.de>
2025-02-17 16:24:39 +0100

    fix(git editor): fix VS code as git editor on linux

--- SetupDeveloperPC.sh
+++ SetupDeveloperPC.sh
@@ -18,7 +18,7 @@ ConfigureGit() {
     git config --local core.autocrlf false
     git config --local core.fscache true
     git config --local core.symlinks false
-    git config --local core.editor "code"
+    git config --local core.editor "code --wait --new-window"
     git config --local alias.graph1 "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
     git config --local alias.graph2 "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
     git config --local alias.graph "!git graph1"
