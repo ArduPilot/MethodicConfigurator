4fa7ffbe11e0d4950d13a97085c2d4edb1b130af
Dr.-Ing. Amilcar do Carmo Lucas <amilcar.lucas@iav.de>
2025-03-08 16:23:21 +0100

    chore(linux): remove install_linux.sh script. Activate argcomplete
    Only developers need to install it and they should use SetupDeveloperPC.sh instead
    
    Clean up licenses

--- install_msgfmt.bat
+++ install_msgfmt.bat
@@ -1,4 +1,7 @@
 @echo off
+rem SPDX-FileCopyrightText: 2024-2025 Amilcar do Carmo Lucas <amilcar.lucas@iav.de>
+rem
+rem SPDX-License-Identifier: GPL-3.0-or-later
 setlocal enabledelayedexpansion
 
 :: Check for admin privileges
