d2b778ab777532d2d593da47926fb80a8106e2e9
Dr.-Ing. Amilcar do Carmo Lucas <amilcar.lucas@iav.de>
2025-03-30 17:13:46 +0200

    fix(configuration steps): Spelling corrections

--- ardupilot_methodic_configurator/configuration_steps_strings.py
+++ ardupilot_methodic_configurator/configuration_steps_strings.py
@@ -22,7 +22,7 @@ def configuration_steps_strings() -> None:  # noqa: PLR0915 # pylint: disable=to
     For pygettext to extract them, they have no other function
     """
     # why
-    _config_steps_strings = _("An even better result for roll and pitch will be achieved by re-autotuning an already (in all axis) autonuned vehicle")
+    _config_steps_strings = _("An even better result for roll and pitch will be achieved by re-autotuning an already (in all axis) autotuned vehicle")
     _config_steps_strings = _("Because the mathematical model of the vehicle allows for analytical PID optimization")
     _config_steps_strings = _("Correct orientation ensures that the flight controller accurately interprets the vehicle's movements and orientation, which is fundamental for stable flight and navigation.")
     _config_steps_strings = _("Ensures the vehicle operates within safe voltage limits and can trigger appropriate failsafe actions to protect the vehicle and its surroundings. It also helps by scaling the PIDs to provide a constant flight behavior, independent of the battery state-of-charge")
@@ -71,7 +71,7 @@ def configuration_steps_strings() -> None:  # noqa: PLR0915 # pylint: disable=to
     _config_steps_strings = _("Because guided mode requires a tunned position controller")
     _config_steps_strings = _("Because it can only be done after PID tuning is complete")
     _config_steps_strings = _("Because it can only be done after the respective autotune flight")
-    _config_steps_strings = _("Because it depends on windspeed estimation")
+    _config_steps_strings = _("Because it depends on wind speed estimation")
     _config_steps_strings = _("Because it is optional and should only be done after most other configurations are complete")
     _config_steps_strings = _("Because it needs an initial autotune of all axis")
     _config_steps_strings = _("Because pitch is usually the axis that has the second highest dynamic, and we continue with the second highest dynamic axis")
@@ -190,8 +190,8 @@ def configuration_steps_strings() -> None:  # noqa: PLR0915 # pylint: disable=to
     _config_steps_strings = _("Remote ID (aka Drone ID)")
     _config_steps_strings = _("Telemetry (landing page)")
     _config_steps_strings = _("Test AltHold")
-    _config_steps_strings = _("Windspeed Estimation")
     _config_steps_strings = _("optical flow sensor setup")
+    _config_steps_strings = _("wind speed estimation")
 
     # external_tool_text
     _config_steps_strings = _("ArduCopter Analytic Tune Tool")
@@ -287,7 +287,7 @@ Jump to '47_position_controller.param' file?""")
     _config_steps_strings = _("Use MOT_THST_HOVER assuming MOT_THST_HOVER has been correctly learned")
     _config_steps_strings = _("Use VTOL-Quicktune lua script to estimate a good PID starting values")
     _config_steps_strings = _("Use acc and gyro batch logging on F4 processors, gyro raw logging on others")
-    _config_steps_strings = _("Use lua scripting for VTOL-Quicktune, MagFit automation and Windspeed Estimation automation")
+    _config_steps_strings = _("Use lua scripting for VTOL-Quicktune, MagFit automation and wind speed estimation automation")
     _config_steps_strings = _("Use pre and post filters acc and gyro batch logging on F4 processors, pre-post gyro raw logging on others")
     _config_steps_strings = _("Use the first notch filter to filter the noise created by the motors/propellers")
     _config_steps_strings = _("Value for the first couple of flights will be changed later once MOT_THST_HOVER is learned")
