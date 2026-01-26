4c0a8a044e95477ea82ad6032b6d9ea0b7749c36
Dr.-Ing. Amilcar do Carmo Lucas <amilcar.do.carmo.lucas@iav.de>
2026-01-26 21:29:06 +0100

    chore(deps): Remove wheel dependency
    It is no longer needed

--- test_pip_package.sh
+++ test_pip_package.sh
@@ -30,11 +30,11 @@ if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
 else
     source .venv/bin/activate
 fi
-uv pip install -U "build==1.3.0" "packaging==25.0" "setuptools==66.1.1" "wheel==0.38.4"
+uv pip install -U "build==1.4.0" "packaging==26.0" "setuptools==80.10.2"
 
 python -m build
 
-uv pip install -U dist/ardupilot_methodic_configurator-2.4.0-py3-none-any.whl
+uv pip install -U dist/ardupilot_methodic_configurator-2.9.6-py3-none-any.whl
 
 cd ..
 
