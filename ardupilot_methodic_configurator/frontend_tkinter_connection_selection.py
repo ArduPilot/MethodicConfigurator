64b3027b8d54f5637a57341f43843d8407f0469d
Dr.-Ing. Amilcar do Carmo Lucas <amilcar.lucas@iav.de>
2026-03-21 14:56:57 +0100

    fix(ui): Make the FC connection window smaller

--- ardupilot_methodic_configurator/frontend_tkinter_connection_selection.py
+++ ardupilot_methodic_configurator/frontend_tkinter_connection_selection.py
@@ -315,8 +315,8 @@ class ConnectionSelectionWindow(BaseWindow):
         default_baudrate: int = 115200,
     ) -> None:
         super().__init__()
-        self.root.title(_("AMC {version} - Flight controller connection").format(version=__version__))  # Set the window title
-        self.root.geometry(self.calculate_scaled_geometry(520, 462))  # Set the window size
+        self.root.title(_("AMC {version} - Flight controller connection").format(version=__version__))
+        self.root.geometry(self.calculate_scaled_geometry(520, 380))
         self.center_window_on_screen(self.root)
         self.default_baudrate = default_baudrate
 
