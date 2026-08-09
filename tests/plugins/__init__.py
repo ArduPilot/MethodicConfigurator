b4286f0d50dd55a2ce8feb1d486a6be422cfc4e6
Dr.-Ing. Amilcar do Carmo Lucas <amilcar.lucas@iav.de>
2026-08-09 18:34:51 +0200

    chore(reuse): add copyrights

--- tests/plugins/__init__.py
+++ tests/plugins/__init__.py
@@ -1,1 +1,10 @@
-"""Tests for the ArduPilot methodic configurator plugins."""
+"""
+Tests for the ArduPilot methodic configurator plugins.
+
+This file is part of ArduPilot Methodic Configurator. https://github.com/ArduPilot/MethodicConfigurator
+
+SPDX-FileCopyrightText: 2024-2026 Amilcar do Carmo Lucas <amilcar.lucas@iav.de>
+
+SPDX-License-Identifier: GPL-3.0-or-later
+
+"""
