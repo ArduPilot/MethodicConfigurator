6a9f12f02d2e80f8b2f7a8932f9f19f9690aef35
Amilcar Lucas <amilcar.lucas@iav.de>
2026-08-26 21:23:49 +0000

    chore(deps): update dependency setuptools to v84

--- test_pip_package.sh
+++ test_pip_package.sh
@@ -30,7 +30,7 @@ if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
 else
     source .venv/bin/activate
 fi
-uv pip install "build==1.5.0" "packaging==26.3" "setuptools==83.0.0"
+uv pip install "build==1.5.0" "packaging==26.3" "setuptools==84.0.0"
 
 python -m build
 
